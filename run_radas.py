ae57eb7d4cae85df97b07eefd48656d5f5954a0c
Christoph Hasse <hassec@users.noreply.github.com>
2023-10-27 11:50:43 -0400

    add Be, Li, N, and process in parallel

--- run_radas.py
+++ run_radas.py
@@ -2,6 +2,7 @@
 # Run this script from the repository directory.
 import click
 import matplotlib.pyplot as plt
+import multiprocessing as mp
 
 from radas.computation import (
     read_cases,
@@ -20,9 +21,10 @@ from radas.unit_handling import ureg
 def run_radas(case: str, show: bool):
 
     if case == "all":
-        for case in read_cases.list_cases():
-            print(f"Running {case}")
-            run_radas_for_case(case)
+        cases = read_cases.list_cases()
+        print(f"Running all cases: {cases}")
+        with mp.Pool() as pool:
+            pool.map(run_radas_for_case, cases)
     else:
         run_radas_for_case(case)
 
@@ -38,14 +40,14 @@ def run_radas_for_case(case: str):
     dataset["coronal_charge_state_fraction"] = calculate_coronal_states.calculate_coronal_states(dataset)
     dataset["coronal_mean_charge_state"] = (dataset.coronal_charge_state_fraction * dataset.dim_charge_state).sum(dim="dim_charge_state")
     dataset["coronal_electron_emission_prefactor"] = calculate_radiation.calculate_electron_emission_prefactor(dataset, dataset.coronal_charge_state_fraction)
-    
+
     dataset["residence_time"] = (dataset.ne_tau / dataset.electron_density).pint.to(ureg.s)
 
     dataset["charge_state_fraction_evolution"] = calculate_derivatives.calculate_time_evolution(dataset)
     dataset["charge_state_fraction_at_equilibrium"] = dataset.charge_state_fraction_evolution.isel(dim_time=-1)
     dataset["noncoronal_mean_charge_state"] = (dataset.charge_state_fraction_at_equilibrium * dataset.dim_charge_state).sum(dim="dim_charge_state")
     dataset["noncoronal_electron_emission_prefactor"] = calculate_radiation.calculate_electron_emission_prefactor(dataset, dataset.charge_state_fraction_at_equilibrium)
-    
+
     (cases_directory / dataset.case / "output").mkdir(exist_ok=True)
 
     write_output(dataset, file_output)
@@ -54,6 +56,7 @@ def run_radas_for_case(case: str):
     figsize, show_dpi, save_dpi = plots.pop("figsize"), plots.pop("show_dpi"), plots.pop("save_dpi")
     for key, plot in plots.items():
         make_plots(dataset, key, plot, figsize, show_dpi, save_dpi)
-    
+
+ 
 if __name__=="__main__":
     run_radas()
