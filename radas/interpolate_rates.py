9d2cd134b3aed07366950ec1abf100efa514ef81
Tom Body <tbody@cfs.energy>
2026-04-22 13:58:20 -0400

    Use nearest-neighbour if extrapolation needed

--- radas/interpolate_rates.py
+++ radas/interpolate_rates.py
@@ -8,6 +8,8 @@ def interpolate_array(array: xr.DataArray, new_electron_density: NDArray[np.floa
     """Interpolate array onto new values for the electron density and electron temp.
     
     The interpolation is performed for logarithmic values.
+
+    Nearest-neighbour extrapolation is used to fill any off-grid points.
     """
     units = array.pint.units
     array = array.pint.dequantify().squeeze()
@@ -27,52 +29,14 @@ def interpolate_array(array: xr.DataArray, new_electron_density: NDArray[np.floa
 
     x_interp = np.log10(new_electron_density)
     y_interp = np.log10(new_electron_temp)
-    z_interp = np.power(10, RectBivariateSpline(x, y, z)(x_interp, y_interp, grid=True).T)
+
+    # Clip the interpolation to exclude off-grid points. This is equivalent to using nearest-neighbour
+    # extrapolation for off-grid values.
+    x_interp_clipped = np.clip(x_interp, x.min().item(), x.max().item())
+    y_interp_clipped = np.clip(y_interp, y.min().item(), y.max().item())
+
+    z_interp = np.power(10, RectBivariateSpline(x, y, z)(x_interp_clipped, y_interp_clipped, grid=True).T)
 
     return xr.DataArray(z_interp,
         coords=dict(dim_electron_temp=new_electron_temp, dim_electron_density=new_electron_density)
     ) * units
-
-def interpolate_dataset(dataset: xr.Dataset, electron_density_resolution: int, electron_temp_resolution: int) -> xr.Dataset:
-    """Interpolate all rate coefficients in a dataset."""
-    new_electron_density = np.logspace(
-        np.log10(dataset["dim_electron_density"].min().item()),
-        np.log10(dataset["dim_electron_density"].max().item()),
-        num = electron_density_resolution
-    )
-
-    new_electron_temp = np.logspace(
-        np.log10(dataset["dim_electron_temp"].min().item()),
-        np.log10(dataset["dim_electron_temp"].max().item()),
-        num = electron_temp_resolution
-    )
-
-    new_dataset = xr.Dataset().assign_attrs(dataset.attrs)
-
-    for key, array in dataset.items():
-        if key in [
-            "electron_density",
-            "electron_temp",
-        ]:
-            # Don't copy in the coordinate arrays which we'll interpolate
-            continue
-        elif key in [
-            "ne_tau"
-        ]:
-            # Directly copy in the coordinate arrays which we'll leave unchanged
-            new_dataset[key] = array
-        elif array.ndim == 0:
-            # Directly copy in scalar arrays
-            new_dataset[key] = array
-        elif (("dim_electron_density" in array.coords)
-              and ("dim_electron_temp" in array.coords)
-              and ("dim_charge_state" in array.coords)):
-            # For each charge state, interpolate the rate coefficient
-            new_dataset[key] = array.groupby("dim_charge_state").map(interpolate_array, args=(new_electron_density, new_electron_temp))
-        else:
-            raise NotImplementedError(f"Could not process array '{key}' with coords {array.coords}")
-    
-    new_dataset["electron_density"] = xr.DataArray(new_electron_density, dims="dim_electron_density") * dataset["electron_density"].pint.units
-    new_dataset["electron_temp"] = xr.DataArray(new_electron_temp, dims="dim_electron_temp") * dataset["electron_temp"].pint.units
-    
-    return new_dataset
